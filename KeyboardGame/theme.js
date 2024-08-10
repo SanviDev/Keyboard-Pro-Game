8d36230e953a601c0c24883d6aa024ce776cffd5
SanviDev <161404667+SanviDev@users.noreply.github.com>
2024-08-10 16:13:22 -0300

    Delete KeyboardGame directory

--- KeyboardGame/theme.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('.fa-regular');
-const body = document.querySelector('body');
-
-button.addEventListener('click', () => {
-    body.classList.toggle('light');
-    button.classList.toggle('fa-sun');
-    button.classList.toggle('fa-moon');
-})
